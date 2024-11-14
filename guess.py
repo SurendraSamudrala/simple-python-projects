cfab26cfd48b6f62d801f212ca17ea10ec6d295f
SurendraSamudrala <91685721+SurendraSamudrala@users.noreply.github.com>
2024-11-14 21:22:25 +0530

    Update guess.py

--- guess.py
+++ guess.py
@@ -2,7 +2,7 @@ import random
 n = random.randint(1,100)
 guess=0
 while guess!=n:
-    guess=int(input('Enter the number :  ')
+    guess=int(input('Enter the number :  '))
     if (guess<n):
         print('Guess number too low')
     elif (guess>n):
